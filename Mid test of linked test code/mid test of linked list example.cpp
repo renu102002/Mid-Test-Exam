d80db836052cb2243248ab933cec43ecca1ac4fe
renu102002 <157440265+renu102002@users.noreply.github.com>
2024-02-19 16:43:02 -0500

    first commit

---
+++ Mid test of linked test code/mid test of linked list example.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+
+// Definition of a Node representing a contact
+struct Contact {
+    std::string name;
+    std::string phoneNumber;
+    Contact* next;
+
+    Contact(std::string contactName, std::string phone)
+        : name(contactName), phoneNumber(phone), next(nullptr) {}
+};
+
+// Class representing a Contact List
+class ContactList {
+public:
+    ContactList() : head(nullptr) {}
+
+    // Function to add a new contact to the list
+    void addContact(std::string contactName, std::string phoneNumber) {
+        Contact* newContact = new Contact(contactName, phoneNumber);
+        newContact->next = head;
+        head = newContact;
+    }
+
+    // Function to display all contacts in the list
+    void displayContacts() {
+        Contact* current = head;
+        while (current != nullptr) {
+            std::cout << "Name: " << current->name << ", Phone: " << current->phoneNumber << std::endl;
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+    // Destructor to release dynamically allocated memory
+    ~ContactList() {
+        Contact* current = head;
+        while (current != nullptr) {
+            Contact* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+private:
+    Contact* head;
+};
+
+int main() {
+    ContactList contacts;
+
+    char choice;
+    do {
+        std::string name, phone;
+        std::cout << "Enter contact name: ";
+        std::cin >> name;
+
+        std::cout << "Enter contact phone number: ";
+        std::cin >> phone;
+
+        contacts.addContact(name, phone);
+
+        std::cout << "Do you want to add another contact? (y/n): ";
+        std::cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    // Displaying contacts
+    std::cout << "Contact List:" << std::endl;
+    contacts.displayContacts();
+
+    // Destructor is automatically called when 'contacts' goes out of scope
+
+    return 0;
+}
---
+++ mid test of binary tree/mid test of binary tree example.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class TrainStation {
+private:
+    std::string name;
+    std::vector<TrainStation*> connections;
+
+public:
+    TrainStation(const std::string& n) : name(n) {}
+
+    void addConnection(TrainStation* station) {
+        connections.push_back(station);
+    }
+
+    std::string getName() const {
+        return name;
+    }
+
+    void print(int depth = 0) const {
+        for (int i = 0; i < depth; ++i)
+            std::cout << "   |";
+
+        std::cout << "---" << name << "\n";
+
+        for (const auto& connection : connections)
+            connection->print(depth + 1);
+    }
+
+    ~TrainStation() {
+        for (auto connection : connections)
+            delete connection;
+    }
+};
+
+// Function to recursively build the train network
+void buildTrainNetwork(TrainStation* currentStation) {
+    int numConnections;
+    std::cout << "Enter the number of connections for station " << currentStation->getName() << ": ";
+    std::cin >> numConnections;
+
+    for (int i = 0; i < numConnections; ++i) {
+        std::string connectionName;
+
+        std::cout << "Enter the name of connected station " << i + 1 << ": ";
+        std::cin >> connectionName;
+
+        TrainStation* connectedStation = new TrainStation(connectionName);
+        currentStation->addConnection(connectedStation);
+
+        // Recursively build the train network for the connected station
+        buildTrainNetwork(connectedStation);
+    }
+}
+
+int main() {
+    std::string centralStationName;
+
+    // Getting information for the central station
+    std::cout << "Enter the name for the central station: ";
+    std::cin >> centralStationName;
+
+    // Creating the central station
+    TrainStation* centralStation = new TrainStation(centralStationName);
+
+    // Building the train network
+    buildTrainNetwork(centralStation);
+
+    // Printing the train network
+    std::cout << "\nTrain Network:\n";
+    centralStation->print();
+
+    // Clean up allocated memory
+    delete centralStation;
+
+    return 0;
+}
